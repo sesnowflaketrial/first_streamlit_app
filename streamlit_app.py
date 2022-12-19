ef46eee9f184ef4d7ca050191613250893f5643c
sesnowflaketrial <120531826+sesnowflaketrial@users.noreply.github.com>
2022-12-20 12:27:10 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,5 +47,5 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit') 
 streamlit.write('Thanks for adding ', add_my_fruit)
 
-#this will not work correctly, but just go wiht it for now
-# my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#this will not work correctly, but just go with it for now
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
