1e5604dc63ad44f2bce8e3e7d98587c08b6580e7
sesnowflaketrial <120531826+sesnowflaketrial@users.noreply.github.com>
2022-12-20 13:42:14 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
 
 # Create the repeatable code block (called a funtion)
 def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return streamlit.dataframe(fruityvice_normalized)  
 
