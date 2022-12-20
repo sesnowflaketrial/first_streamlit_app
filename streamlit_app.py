883ff65fd5337210596dc32d52594283e34f2181
sesnowflaketrial <120531826+sesnowflaketrial@users.noreply.github.com>
2022-12-20 13:02:22 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,9 @@ try:
   else:
       back_from_function = get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
-    
+# don't run anything past here while we troubleshoot
+streamlit.stop()
+
 except URLError as e:
     streamlit.error()
 
