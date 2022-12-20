c2fe0d9b9b92f24fa84ae073ac9e7fe3eacf6b8a
sesnowflaketrial <120531826+sesnowflaketrial@users.noreply.github.com>
2022-12-20 13:51:36 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ def get_fruityvice_data(this_fruit_choice):
     return fruityvice_normalized
 
 # New Section to dispaly fruitvice api response
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
@@ -44,7 +44,7 @@ try:
 except URLError as e:
     streamlit.error()
 
-streamlit.header("The fruit load list contains:")
+streamlit.header('The fruit load list contains:')
 #snowflake-related funtions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
